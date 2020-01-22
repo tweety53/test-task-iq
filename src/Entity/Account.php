204bdf0388e3e0cc0aad3a3433ce03a604ef7cdd
tweety53 <yatweety@gmail.com>
2020-01-22 21:42:55 +0300

    Emoji check lul :rotating_light:

--- src/Entity/Account.php
+++ src/Entity/Account.php
@@ -19,14 +19,40 @@ class Account
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    private $userId;
+    private int $userId;
 
     /**
      * @var string
      *
      * @ORM\Column(name="amount", type="decimal", precision=10, scale=2, nullable=false, options={"default"="0.00"})
      */
-    private $amount = '0.00';
+    private string $amount = '0.00';
 
+    /**
+     * @return int
+     */
+    public function getUserId(): int
+    {
+        return $this->userId;
+    }
+
+
+    /**
+     * @return string
+     */
+    public function getAmount(): string
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param string $amount
+     * @return Account
+     */
+    public function setAmount(string $amount): Account
+    {
+        $this->amount = $amount;
+        return $this;
+    }
 
 }
--- src/Entity/OperationHistory.php
+++ src/Entity/OperationHistory.php
@@ -12,6 +12,12 @@ use Doctrine\ORM\Mapping as ORM;
  */
 class OperationHistory
 {
+    public const TYPE_DEPOSIT = 1;
+    public const TYPE_WITHDRAWAL = 2;
+    public const TYPE_TRANSFER = 3;
+
+    public const TYPE_UNKNOWN = 4;
+
     /**
      * @var int
      *
@@ -19,41 +25,113 @@ class OperationHistory
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
      */
-    private $id;
+    private int $id;
 
     /**
      * @var int
      *
      * @ORM\Column(name="type", type="smallint", nullable=false)
      */
-    private $type;
+    private int $type;
 
     /**
      * @var string
      *
      * @ORM\Column(name="amount", type="decimal", precision=10, scale=2, nullable=false)
      */
-    private $amount;
+    private string $amount;
 
     /**
-     * @var \Account
+     * @var Account|null
      *
      * @ORM\ManyToOne(targetEntity="Account")
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="user_from", referencedColumnName="user_id")
      * })
      */
-    private $userFrom;
+    private ?Account $userFrom;
 
     /**
-     * @var \Account
+     * @var Account|null
      *
      * @ORM\ManyToOne(targetEntity="Account")
      * @ORM\JoinColumns({
      *   @ORM\JoinColumn(name="user_to", referencedColumnName="user_id")
      * })
      */
-    private $userTo;
+    private ?Account $userTo;
+
+    /**
+     * @return int
+     */
+    public function getType(): int
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param int $type
+     * @return OperationHistory
+     */
+    public function setType(int $type): OperationHistory
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAmount(): string
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param string $amount
+     * @return OperationHistory
+     */
+    public function setAmount(string $amount): OperationHistory
+    {
+        $this->amount = $amount;
+        return $this;
+    }
+
+    /**
+     * @return Account
+     */
+    public function getUserFrom(): Account
+    {
+        return $this->userFrom;
+    }
+
+    /**
+     * @param Account $userFrom
+     * @return OperationHistory
+     */
+    public function setUserFrom(Account $userFrom): OperationHistory
+    {
+        $this->userFrom = $userFrom;
+        return $this;
+    }
+
+    /**
+     * @return Account
+     */
+    public function getUserTo(): Account
+    {
+        return $this->userTo;
+    }
+
+    /**
+     * @param Account $userTo
+     * @return OperationHistory
+     */
+    public function setUserTo(Account $userTo): OperationHistory
+    {
+        $this->userTo = $userTo;
+        return $this;
+    }
 
 
 }
